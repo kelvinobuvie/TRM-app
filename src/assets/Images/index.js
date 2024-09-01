be7b95c15c511ffbc78e588384d0aa4c0898469c
kelvinobuvie <Kelvinobuvie@gmail.com>
2024-09-01 19:34:26 +0100

    done with the picture side of the login

--- src/assets/Images/index.js
+++ src/assets/Images/index.js
@@ -1,5 +1,6 @@
 import Logo from './LAC logo Alt 2.svg';
 import Signin from './New Leadway Logo 1.png';
 import Loginpic from './Mask Group.png'
+import Group from './_Group_.png'
 
-export {Logo, Signin, Loginpic} 
+export {Logo, Signin, Loginpic, Group} 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import {Signin} from "../assets/Images"
+import {Group, Signin} from "../assets/Images"
 import Input from '../components/Input';
 import { LuUser2 } from 'react-icons/lu';
 import { GoLock } from "react-icons/go";
+import { GiHelp } from "react-icons/gi";
 
 const Login = () => {
   return (
-    <div className='flex w-full'>
-       <div className="sideA w-1/2 h-[100vh] bg-white pt-10 px-20 pb-24 justify-self-center">
+    <div className='flex w-full bg-orange-400 box-content'>
+       <div className="sideA  w-1/2 h-full bg-white pt-10 px-20 pb-24 justify-self-center">
             <div className=''>
                 <div> 
                   <img src={Signin} alt="ndnddj w-3 h-3"/>
@@ -34,11 +35,27 @@ const Login = () => {
                   <a href=''>Keep me logged in</a>
                   <a href=''>Forgort password</a>
             </div>
-            </div>
-           
+            </div>   
        </div>
-       <div className="sideB w-1/2 h-[100vh]">
-            
+       <div className="sideB w-1/2 h-full">
+       <div className='flex flex-col justify-between items-center  h-full py-[39px]'>
+        	<div className='flex text-white mr-[51px] text-lg text-bold gap-1 self-end'>
+              <a href="" className='font-semibold'>
+              <GiHelp className='inline-block mr-2 -mt-1'/>
+                Help</a>
+          </div>
+          <div className='loginb flex w-[500px] h-[115px] bg-white rounded-md '>
+              <div className='py-[20px] px-5 bg-gray-200'>
+                <img className='w-[72px] h-[72px]' src={Group} alt=""/>
+              </div>
+              <div className='py-5 pl-[30px]'>
+                <p className='font-bold text-s'>Customer Management</p>
+                <p className='mt-3 text-xs'>Manage all customers from individual customers to corporate
+                or group customers.</p>
+              </div>
+            </div>
+       </div>
+           
        </div>
     </div>
   )
